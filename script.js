03e50acb3b5e779e8ef4960f7b7f6a3bc0cbc19c
Mishael_eth <henrymishael.hm@gmail.com>
2023-03-16 20:20:59 +0100

    feat:add new files

---
+++ script.js
@@ -0,0 +1,33 @@
+//data type
+// let, const,var
+
+// let frontendIntern = 'mishael'
+
+// let intern ='mishael'
+// intern = 'henry'
+
+// let frontend = `corporatenerd ${intern}`
+
+// let name1 = 'mishael'
+// let age = 22
+// let statement =`my name is ${frontend} and i was born in ${new Date().getFullYear() - age}`
+// let x ='mishael'
+
+let num12 = '8'
+// let wee = 'he says "his name is henry'
+// alert(wee)
+
+let wee = 'he says \'his name is henry'
+// alert(wee)
+
+
+let intern = 'mishael'
+intern = 'Onyekachukwu'
+
+let age = '22'
+
+const number = '09063136386'
+
+let statement = `my name is ${intern} and i am ${age} years and you can contact me on ${number}`
+
+// alert(statement)
